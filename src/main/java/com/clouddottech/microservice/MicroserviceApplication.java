c2ec2170ce3a38bb2384fc40bfae099698b89453
First Last <email.mail.com>
2021-05-09 10:27:04 -0500

    first commit

--- src/main/java/com/clouddottech/microservice/MicroserviceApplication.java
+++ src/main/java/com/clouddottech/microservice/MicroserviceApplication.java
@@ -1,13 +1,26 @@
 package com.clouddottech.microservice;
 
+import javafx.application.Application;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
-public class MicroserviceApplication {
+public class MicroserviceApplication   {
+    @Autowired
+    private MicroserviceApplicationService service;
+
+    private static SpringApplicationBuilder configureApplication(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class).bannerMode(Banner.Mode.OFF);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(MicroserviceApplication.class, args);
@@ -16,5 +29,18 @@ public class MicroserviceApplication {
     @RequestMapping(value = "/")
     public String hello() {
         return "Hello Microservice Spring boot with gradle!";
+    }
+
+    @RequestMapping("/greeting")
+    public @ResponseBody
+    String greeting() {
+        return service.greet();
+    }
+
+    @Service
+    public class MicroserviceApplicationService {
+        public String greet() {
+            return null;
+        }
     }
 }
